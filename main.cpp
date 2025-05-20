1ff6fe4e75d40a2f1034cde4e1c008bf30d9c33e
kuldeep1203 <kuldeeppawar427@gmail.com>
2025-05-20 13:40:54 +0530

    added cpu play and point board

--- main.cpp
+++ main.cpp
@@ -1,57 +1,159 @@
-#include<raylib.h>
-#include<iostream>
+#include <raylib.h>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
+const int screenwidth = 800;
+const int screenheight = 800;
+
 class Ball {
-    public:
-        int ball_x, ball_y;
-        int ball_speed_x, ball_speed_y;
-        int ball_radius;
+public:
+    int ball_x, ball_y;
+    int ball_speed_x, ball_speed_y;
+    int ball_radius;
+
+    Ball(int s, int w) {
+        ball_x = s / 2;
+        ball_y = w / 2;
+        ball_speed_x = 5;
+        ball_speed_y = 5;
+        ball_radius = 15;
+    }
+
     
-        Ball(int s ,int w) {
-            ball_x = s;
-            ball_y = w;
-            ball_speed_x = 5;
-            ball_speed_y = 5;
-            ball_radius = 15;
+    int Update(Rectangle leftPaddle, Rectangle rightPaddle) {
+        ball_x += ball_speed_x;
+        ball_y += ball_speed_y;
+
+        if (ball_y + ball_radius >= screenheight || ball_y - ball_radius <= 0) {
+            ball_speed_y *= -1;
         }
+
+       
+        if (CheckCollisionCircleRec({(float)ball_x, (float)ball_y}, ball_radius, leftPaddle) ||
+            CheckCollisionCircleRec({(float)ball_x, (float)ball_y}, ball_radius, rightPaddle)) {
+            ball_speed_x *= -1;
+        }
+
+     
+        if (ball_x - ball_radius > screenwidth) {
+            return 1;
+        }
+
+      
+        if (ball_x + ball_radius < 0) {
+            return 2;
+        }
+
+        return 0;
+    }
+
+    void Reset() {
+        ball_x = screenwidth / 2;
+        ball_y = screenheight / 2;
+        ball_speed_x = (ball_speed_x > 0) ? -5 : 5; 
+        ball_speed_y = 5;
+    }
 };
+
+class Paddle {
+    int PaddlePoint;
+public:
+    Rectangle paddle;
+   
+
+    Paddle() {
+        paddle = {0, screenheight / 2.0f - 50, 10, 100};
+        PaddlePoint = 0;
+    }
+    int getPoint(){
+        return PaddlePoint;
+    }
+    void Point(){
+        PaddlePoint++;
+    }
+
+    void Movement() {
+     
+        if (IsKeyDown(KEY_W)) {
+            paddle.y -= 5;
+            if (paddle.y < 0) paddle.y = 0;
+        }
+        if (IsKeyDown(KEY_S)) {
+            paddle.y += 5;
+            if (paddle.y + paddle.height > screenheight)
+                paddle.y = screenheight - paddle.height;
+        }
+    }
+};
+
+class CpuPaddle : public Paddle{
+    int CpuPoint;
+    public:
     
-int main(){
+    CpuPaddle() {
+        CpuPoint=0;
+        paddle = {screenwidth - 10, screenheight / 2.0f - 50, 10, 100};
+    }
+    void Movement(int ballY) {
+        float paddleCenterY = paddle.y + paddle.height / 2.0f;
 
-    const int screenwidth = 800;
-    const int screenheight = 800;
+        if (ballY < paddleCenterY - 10) {
+            paddle.y -= 4;
+            if (paddle.y < 0) paddle.y = 0;
+        } else if (ballY > paddleCenterY + 10) {
+            paddle.y += 4;
+            if (paddle.y + paddle.height > screenheight)
+                paddle.y = screenheight - paddle.height;
+        }
+    }
+    int getPoint(){
+        return CpuPoint;
+    }
+    void Point(){
+        CpuPoint++;
+    }
 
 
-    
+};
 
-    InitWindow(screenwidth,screenheight,"test window");
 
+int main() {
+    InitWindow(screenwidth, screenheight, "Pong Game");
     SetTargetFPS(60);
-    Ball newBall(screenwidth,screenheight);
-    while(!WindowShouldClose()){
+
+    Ball newBall(screenwidth, screenheight);
+    Paddle paddle;
+    CpuPaddle paddle2;
+    while (!WindowShouldClose()) {
+        paddle.Movement();
+        paddle2.Movement(newBall.ball_y);
+        int scoreUpdate = newBall.Update(paddle.paddle, paddle2.paddle);
+        if (scoreUpdate == 1) {
+            paddle.Point();
+            newBall.Reset();
+        } else if (scoreUpdate == 2) {
+            paddle2.Point();
+            newBall.Reset();
+        }
+
         BeginDrawing();
         ClearBackground(BLACK);
-        // newBall.ball_x+=newBall.ball_speed_x;
-        // newBall.ball_y+=newBall.ball_speed_y;
 
-        // if(newBall.ball_x + newBall.ball_radius >= screenwidth || newBall.ball_x-newBall.ball_radius <=0 ){
-        //     newBall.ball_speed_x*=-1;
-        // }
-        // if(newBall.ball_y +newBall.ball_radius>=screenheight || newBall.ball_y-newBall.ball_radius<=0 ){
-        //     newBall.ball_speed_y*=-1;
-        // }
+        DrawLine(screenwidth / 2, 0, screenwidth / 2, screenheight, WHITE);
 
-        DrawRectangle(790,screenheight/2,10,100,WHITE);
-        DrawRectangle(0,screenheight/2,10,100,WHITE);
-        DrawCircle(newBall.ball_x/2,newBall.ball_y/2,newBall.ball_radius,WHITE);
+        DrawRectangleRec(paddle.paddle, WHITE);
+        DrawRectangleRec(paddle2.paddle, WHITE);
+
+        DrawText(to_string(paddle.getPoint()).c_str(), screenwidth / 4, 2, 50, WHITE);
+        DrawText(to_string(paddle2.getPoint()).c_str(), 3 * screenwidth / 4, 2, 50, WHITE);
+
+        DrawCircle(newBall.ball_x, newBall.ball_y, newBall.ball_radius, WHITE);
 
         EndDrawing();
     }
+
     CloseWindow();
-
     return 0;
-
-
 }
